797cf597153e8b917e5053fe6103dafbae222076
Nilson <nilzon911@hotmail.com>
2020-09-18 02:39:55 -0300

    segundo commit: version de prueba 1.0.0.2

--- app/src/main/java/com/nilson/proyectoaplicacion/MainActivity.java
+++ app/src/main/java/com/nilson/proyectoaplicacion/MainActivity.java
@@ -3,12 +3,38 @@ package com.nilson.proyectoaplicacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnagregarhora;
+    private  Button btnverhora;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    btnagregarhora = findViewById(R.id.btnagregarhora);
+    btnverhora = findViewById(R.id.btnverhora);
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
 }
