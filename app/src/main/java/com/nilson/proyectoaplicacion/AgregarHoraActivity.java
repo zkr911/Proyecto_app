247be4193bf25297baa3846a9692589797466117
Nilson <nilzon911@hotmail.com>
2020-09-22 00:32:31 -0300

    tercer commit: version de prueba 1.0.0.3 se agrego una ventana

---
+++ app/src/main/java/com/nilson/proyectoaplicacion/AgregarHoraActivity.java
@@ -0,0 +1,41 @@
+package com.nilson.proyectoaplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class AgregarHoraActivity extends AppCompatActivity {
+
+
+   EditText fechainicial;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_hora);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/nilson/proyectoaplicacion/MainActivity.java
+++ app/src/main/java/com/nilson/proyectoaplicacion/MainActivity.java
@@ -2,7 +2,9 @@ package com.nilson.proyectoaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
     btnverhora = findViewById(R.id.btnverhora);
 
 
+
+
+
+    btnagregarhora.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+
+            Intent intento_agregar = new Intent(MainActivity.this,AgregarHoraActivity.class);
+            startActivity(intento_agregar);
+
+        }
+    });
 
 
 
